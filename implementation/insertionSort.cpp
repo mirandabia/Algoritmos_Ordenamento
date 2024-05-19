9c1e5271e2954a7c8bef14d2ce9e61a1431c57e7
Beatriz-mirandab <mirandabeatriz1508@gmail.com>
2024-05-19 11:06:53 -0300

    atualiza insertionSort

--- implementation/insertionSort.cpp
+++ implementation/insertionSort.cpp
@@ -2,48 +2,82 @@
 #include <chrono>
 #include <random>
 
-using std::chrono::high_resolution_clock;
-using std::chrono::duration_cast;
-using std::chrono::microseconds;
+using namespace std;
+using namespace std::chrono;
 
-using std::cout;
-using std::endl;
-
-typedef struct Node 
+struct Node 
 {
     int iPayload; 
     Node* ptrNext;
     Node* ptrPrev;
-} Node;
+};
 
-Node* createNode(int);
-void displayList(Node*);
-void insertEnd(Node**, int);
-void deleteList(Node**);
-void swapValue(Node**, Node**);
-
-void insertionSort(Node**);
-double mean(float*, int);
-double var(float*, int, double);
-
-int main()
+Node* createNode(int iValue) 
 {
-    Node* list1 = nullptr;
-    int iTestLength = 15;
-    for (int iPos = 0; iPos < iTestLength; iPos++)
+    Node* ptrNode = new Node;
+    ptrNode->iPayload = iValue;
+    ptrNode->ptrNext = nullptr;
+    ptrNode->ptrPrev = nullptr;
+    
+    return ptrNode;
+}
+
+void displayList(Node* ptrNode) 
+{
+    if (ptrNode == nullptr)
     {
-        int iRandomVal = rand() % 100 + 1;
-        insertEnd(&list1, iRandomVal);
+        cout << "Lista vazia: não é possível realizar displayList" << endl;
+        return;
+    }
+    
+    if (ptrNode->ptrPrev != nullptr)
+    {
+        cout << "O elemento está no meio ou fim da lista: não é possível realizar displayList" << endl;
+        return;
+    }
+    
+    Node* ptrTemp = ptrNode;
+    cout << "Payload: ";
+    while (ptrTemp != nullptr)
+    {
+        cout << ptrTemp->iPayload << " ";
+        ptrTemp = ptrTemp->ptrNext;
+    }
+    cout << endl;
+}
+
+void insertEnd(Node** ptrHead, int iValue)
+{
+    Node* ptrNewNode = createNode(iValue);
+    if (*ptrHead == nullptr) 
+    {
+        *ptrHead = ptrNewNode; 
+        return;
+    }
+    
+    Node* ptrTemp = *ptrHead;
+    while (ptrTemp->ptrNext != nullptr)
+    {
+        ptrTemp = ptrTemp->ptrNext;
+    }
+    
+    ptrNewNode->ptrPrev = ptrTemp;
+    ptrTemp->ptrNext = ptrNewNode;
+}
+
+void deleteList(Node** ptrHead) 
+{
+    Node* ptrCurrent = *ptrHead;
+    Node* ptrNext = nullptr;
+
+    while (ptrCurrent != nullptr) 
+    {
+        ptrNext = ptrCurrent->ptrNext;
+        delete ptrCurrent; // Usando delete ao invés de free
+        ptrCurrent = ptrNext;
     }
 
-    cout << "Lista desorganizada: " << endl;
-    displayList(list1);
-
-    cout << endl << "Insertion Sort:" << endl;
-    insertionSort(&list1);
-    displayList(list1);
-
-    cout << endl << endl;
+    *ptrHead = nullptr;
 }
 
 void insertionSort(Node** ptrHead)
@@ -51,146 +85,60 @@ void insertionSort(Node** ptrHead)
     if (*ptrHead == nullptr || (*ptrHead)->ptrNext == nullptr)
         return;
 
-    Node* sorted = nullptr;
-    Node* current = *ptrHead;
-    while (current != nullptr)
+    Node* ptrSorted = nullptr;
+    Node* ptrCurrent = *ptrHead;
+    while (ptrCurrent != nullptr)
     {
-        Node* next = current->ptrNext;
-        // Desconecta o nó atual da lista original
-        if (current->ptrPrev)
-            current->ptrPrev->ptrNext = current->ptrNext;
-        if (current->ptrNext)
-            current->ptrNext->ptrPrev = current->ptrPrev;
+        Node* ptrNext = ptrCurrent->ptrNext;
+        if (ptrCurrent->ptrPrev)
+            ptrCurrent->ptrPrev->ptrNext = ptrCurrent->ptrNext;
+        if (ptrCurrent->ptrNext)
+            ptrCurrent->ptrNext->ptrPrev = ptrCurrent->ptrPrev;
 
-        // Inserção na lista ordenada
-        if (sorted == nullptr || sorted->iPayload >= current->iPayload)
+        if (ptrSorted == nullptr || ptrSorted->iPayload >= ptrCurrent->iPayload)
         {
-            current->ptrNext = sorted;
-            current->ptrPrev = nullptr;
-            if (sorted)
-                sorted->ptrPrev = current;
-            sorted = current;
+            ptrCurrent->ptrNext = ptrSorted;
+            ptrCurrent->ptrPrev = nullptr;
+            if (ptrSorted)
+                ptrSorted->ptrPrev = ptrCurrent;
+            ptrSorted = ptrCurrent;
         }
         else
         {
-            Node* temp = sorted;
-            while (temp->ptrNext != nullptr && temp->ptrNext->iPayload < current->iPayload)
+            Node* ptrTemp = ptrSorted;
+            while (ptrTemp->ptrNext != nullptr && ptrTemp->ptrNext->iPayload < ptrCurrent->iPayload)
             {
-                temp = temp->ptrNext;
+                ptrTemp = ptrTemp->ptrNext;
             }
-            current->ptrNext = temp->ptrNext;
-            current->ptrPrev = temp;
-            if (temp->ptrNext)
-                temp->ptrNext->ptrPrev = current;
-            temp->ptrNext = current;
+            ptrCurrent->ptrNext = ptrTemp->ptrNext;
+            ptrCurrent->ptrPrev = ptrTemp;
+            if (ptrTemp->ptrNext)
+                ptrTemp->ptrNext->ptrPrev = ptrCurrent;
+            ptrTemp->ptrNext = ptrCurrent;
         }
-        current = next;
-        }
-    *ptrHead = sorted;
+        ptrCurrent = ptrNext;
+    }
+    *ptrHead = ptrSorted;
 }
 
-
-Node* createNode(int iValue)
+int main()
 {
-    Node* temp = (Node*)malloc(sizeof(Node)); // Manteve o uso de malloc
-    temp->iPayload = iValue;
-    temp->ptrNext = nullptr;
-    temp->ptrPrev = nullptr;
-    
-    return temp;
-}
-
-
-void displayList(Node* node)
-{
-    if (node == nullptr)
+    Node* ptrList1 = nullptr;
+    int iTestLength = 15;
+    for (int iPos = 0; iPos < iTestLength; iPos++)
     {
-        cout << "Lista vazia: não é possível realizar displayList" << endl;
-        return;
-    }
-    
-    // se receber um nó do meio, rejeito (um modo)
-    if (node->ptrPrev != nullptr)
-    {
-        cout << "O elemento está no meio ou fim da lista: não é possível realizar displayList" << endl;
-        return;
-    }
-    
-    Node* temp = node;
-
-    cout << "Payload: ";
-    
-    while (temp != nullptr)
-    {
-        cout <<  temp->iPayload << " ";
-        temp = temp->ptrNext;
-    }
-    
-    cout << endl;
-}
-
-
-void insertEnd(Node** head, int iValue)
-{
-    Node* newNode = createNode(iValue);
-
-    if (*head == nullptr) 
-    {
-        *head = newNode; 
-        return;
-    }
-    
-    Node* temp = *head;
-    while (temp->ptrNext != nullptr)
-    {
-        temp = temp->ptrNext;
-    }
-    
-    newNode->ptrPrev = temp;
-    temp->ptrNext = newNode;
-}
-
-
-void deleteList(Node** head) 
-{
-    Node* current = *head;
-    Node* next;
-
-    while (current != nullptr) {
-        next = current->ptrNext;
-        free(current);
-        current = next;
+        int iRandomVal = rand() % 100 + 1;
+        insertEnd(&ptrList1, iRandomVal);
     }
 
-    *head = nullptr;
-}
+    cout << "Lista desorganizada: " << endl;
+    displayList(ptrList1);
 
+    cout << endl << "Insertion Sort:" << endl;
+    insertionSort(&ptrList1);
+    displayList(ptrList1);
 
-double mean(float* ptrArrf, int iLength) {
-    double llSoma = 0;
+    deleteList(&ptrList1);
 
-    for (int i = 0; i < iLength; i++)
-    {
-        llSoma +=  *(ptrArrf + i);
-    } 
-
-    double dMean = llSoma / static_cast<double>(iLength);
-
-    return dMean;
-}
-
-
-double var(float* ptrArrf, int iLength, double dMean) {
-    double llSoma = 0;
-
-    for (int i = 0; i < iLength; i++)
-    {
-        double iDiff = *(ptrArrf + i) - dMean;
-        
-        llSoma += iDiff * iDiff;
-    } 
-
-    double dVar = llSoma / static_cast<double>(iLength);
-
-    return dVar;
+    return 0;
 }
