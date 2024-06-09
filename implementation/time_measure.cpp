a00b202fc993ed56ea8cba586155336a0298fe07
Beatriz-mirandab <mirandabeatriz1508@gmail.com>
2024-06-09 20:09:05 -0300

    implementa measureExecutionTime

--- implementation/time_measure.cpp
+++ implementation/time_measure.cpp
@@ -1,1 +1,17 @@
 #include "../headers/time_measure.h"
+
+using namespace std;
+using namespace chrono;
+
+int measureExecutionTime(function<void()> func) {
+    // Tempo de início
+    auto timeStart = high_resolution_clock::now();
+    
+    func(); // Execução da função
+    
+    // Tempo de fim
+    auto timeStop = high_resolution_clock::now();
+    
+    int timeDuration = duration_cast<microseconds>(timeStop - timeStart).count();
+    return timeDuration;
+}
