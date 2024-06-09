abfa7467d9280f54be9a46d37fbdfb3a6d76ba8b
Beatriz-mirandab <mirandabeatriz1508@gmail.com>
2024-06-09 20:05:59 -0300

    atualiza header tree

--- headers/tree.h
+++ headers/tree.h
@@ -1,4 +1,20 @@
 #ifndef TREE_H
 #define TREE_H
 
+#include <iostream>
+#include "doublyLinkedList.h"
+
+using std::cout;
+using std::endl;
+using std::cerr;
+
+Node* createNode(int);
+Node* createTreeByArray(int[], int);
+Node* insertNode(Node*, int);
+void destroyTree(Node*);
+void deleteNode(Node*);
+Node* lesserLeaf(Node*);
+Node* higherLeaf(Node*);
+void printTree(Node*);
+
 #endif // TREE_H
